483b1828c2e4eaa975065a9e2d59408f2d95c9ab
Terry Tong <dooshtheboss1@gmail.com>
2025-09-23 00:27:44 -0400

    added different experiments w/ different models

--- src/exps_performance/run_performance.sh
+++ src/exps_performance/run_performance.sh
@@ -19,6 +19,31 @@
 # done
 
 seeds=(0 1 2 3 4)
+
+
+for seed in ${seeds[@]}; do
+  pixi run python cot.py \
+  --backend vllm \
+  --model mistralai/Ministral-8B-Instruct-2410 \
+  --hf_dtype float16 \
+  --hf_device_map auto \
+  --vllm_tensor_parallel 8 \
+  --n 2000 --digits 2 4 8 16 32 --kinds add sub mul lcs knap rod ilp_prod ilp_assign ilp_partition \
+  --outdir execution_ministral --exec_code --batch_size 64
+done
+
+for seed in ${seeds[@]}; do
+  pixi run python cot.py \
+  --backend vllm \
+  --model togethercomputer/Pythia-Chat-Base-7B \
+  --hf_dtype float16 \
+  --hf_device_map auto \
+  --vllm_tensor_parallel 8 \
+  --n 2000 --digits 2 4 8 16 32 --kinds add sub mul lcs knap rod ilp_prod ilp_assign ilp_partition \
+  --outdir execution_pythia --exec_code --batch_size 64
+done
+
+
 for seed in ${seeds[@]}; do
   pixi run python cot.py \
     --backend vllm \
@@ -29,6 +54,74 @@ for seed in ${seeds[@]}; do
     --n 2000 --digits  2 4 8 16 32 --kinds add sub mul lcs knap rod ilp_prod ilp_assign ilp_partition \
     --outdir deepseek_execution_again --exec_code --batch_size 64 --seed ${seed}
 done
+
+for seed in ${seeds[@]}; do
+  pixi run python cot.py \
+    --backend vllm \
+    --model google/gemma-2-9b-it \
+    --hf_dtype float16 \
+    --hf_device_map auto \
+    --n 2000 --digits 2 4 8 16 32 --kinds add sub mul lcs knap rod ilp_prod ilp_assign ilp_partition \
+    --outdir execution_gemma --exec_code --batch_size 64 --seed ${seed}
+done
+
+for seed in ${seeds[@]}; do
+  pixi run python cot.py \
+    --backend vllm \
+    --model google/gemma-2-9b-it \
+    --hf_dtype float16 \
+    --hf_device_map auto \
+    --n 2000 --digits 2 4 8 16 32 --kinds add sub mul lcs knap rod ilp_prod ilp_assign ilp_partition \
+    --outdir execution_gemma --exec_code --batch_size 64 --seed ${seed}
+done
+
+for seed in ${seeds[@]}; do
+  pixi run python cot.py \
+  --backend vllm \
+  --model meta-llama/Llama-3.1-8B-Instruct \
+  --hf_dtype float16 \
+  --hf_device_map auto \
+  --n 2000 --digits 2 4 8 16 32 --kinds add sub mul lcs knap rod ilp_prod ilp_assign ilp_partition \
+  --outdir execution_llama --exec_code --batch_size 64
+done
+
+for seed in ${seeds[@]}; do
+  pixi run python cot.py \
+  --backend vllm \
+  --model Qwen/Qwen2.5-Coder-7B-Instruct \
+  --hf_dtype float16 \
+  --hf_device_map auto \
+  --vllm_tensor_parallel 1 \
+  --n 2000 --digits 2 4 8 16 32 --kinds add sub mul lcs knap rod ilp_prod ilp_assign ilp_partition \
+  --outdir out_hf_scale_qwen --exec_code --batch_size 8
+done
+
+for seed in ${seeds[@]}; do
+  pixi run python cot.py \
+  --backend vllm \
+  --model mistralai/Ministral-8B-Instruct-2410 \
+  --hf_dtype float16 \
+  --hf_device_map auto \
+  --vllm_tensor_parallel 8 \
+  --n 2000 --digits 2 4 8 16 32 --kinds add sub mul lcs knap rod ilp_prod ilp_assign ilp_partition \
+  --outdir execution_ministral --exec_code --batch_size 64
+done
+
+for seed in ${seeds[@]}; do
+  pixi run python cot.py \
+  --backend vllm \
+  --model togethercomputer/Pythia-Chat-Base-7B \
+  --hf_dtype float16 \
+  --hf_device_map auto \
+  --vllm_tensor_parallel 8 \
+  --n 2000 --digits 2 4 8 16 32 --kinds add sub mul lcs knap rod ilp_prod ilp_assign ilp_partition \
+  --outdir execution_pythia --exec_code --batch_size 64
+done
+
+
+
+
+  
 
 # seeds=(0 1 2 3 4)
 # for seed in ${seeds[@]}; do
@@ -75,8 +168,8 @@ done
 #     --model google/gemma-2-9b-it \
 #     --hf_dtype float16 \
 #     --hf_device_map auto \
-#     --n 2000 --digits 2 4 8 16 --kinds add sub mul lcs knap rod ilp_prod ilp_assign ilp_partition \
-#     --outdir out_hf_scale_gemma_seeded_nlfix --exec_code --batch_size 64 --seed ${seed}
+#     --n 2000 --digits 2 4 8 16 32 --kinds add sub mul lcs knap rod ilp_prod ilp_assign ilp_partition \
+#     --outdir execution_gemma --exec_code --batch_size 64 --seed ${seed}
 # done
 
 # for seed in ${seeds[@]}; do
@@ -109,21 +202,21 @@ done
 #   --n 10000 --digits 2 4 8 16 32 --kinds add sub mul lcs knap rod ilp_prod ilp_assign ilp_partition \
 #   --outdir out_hf_scale_olmo --exec_code --batch_size 64
 
-# pixi run python cot.py \
-#   --backend vllm \
-#   --model meta-llama/Llama-3.1-8B-Instruct \
-#   --hf_dtype float16 \
-#   --hf_device_map auto \
-#   --n 10000 --digits 2 4 8 16 32 --kinds add sub mul lcs knap rod ilp_prod ilp_assign ilp_partition \
-#   --outdir out_hf_scale_meta_llama --exec_code --batch_size 64
+pixi run python cot.py \
+  --backend vllm \
+  --model meta-llama/Llama-3.1-8B-Instruct \
+  --hf_dtype float16 \
+  --hf_device_map auto \
+  --n 10000 --digits 2 4 8 16 32 --kinds add sub mul lcs knap rod ilp_prod ilp_assign ilp_partition \
+  --outdir out_hf_scale_meta_llama --exec_code --batch_size 64
 
-# pixi run python cot.py \
-#   --backend vllm \
-#   --model Qwen/Qwen2.5-Coder-7B-Instruct \
-#   --hf_dtype float16 \
-#   --hf_device_map auto \
-#   --n 10000 --digits 2 4 8 16 32 --kinds add sub mul lcs knap rod ilp_prod ilp_assign ilp_partition \
-#   --outdir out_hf_scale_qwen --exec_code --batch_size 64
+pixi run python cot.py \
+  --backend vllm \
+  --model Qwen/Qwen2.5-Coder-7B-Instruct \
+  --hf_dtype float16 \
+  --hf_device_map auto \
+  --n 10000 --digits 2 4 8 16 32 --kinds add sub mul lcs knap rod ilp_prod ilp_assign ilp_partition \
+  --outdir out_hf_scale_qwen --exec_code --batch_size 64
 
   
 
