58dd32d689f8a0346862d9a85b0b76f3f620cc7d
Terry Tong <dooshtheboss1@gmail.com>
2025-09-23 19:58:54 -0400

    adding 1 shot trials

--- src/exps_performance/run_performance.sh
+++ src/exps_performance/run_performance.sh
@@ -1,57 +1,29 @@
 
-# pixi run python cot.py \
-#   --backend vllm \
-#   --model  google/gemma-2-9b-it \
-#   --hf_dtype float16 \
-#   --hf_device_map auto \
-#   --n 10000 --digits 2 4 8 16 32 --kinds add sub mul lcs knap rod ilp_prod ilp_assign ilp_partition \
-#   --outdir out_hf_scale_gemma_rerun --exec_code --batch_size 64
-
-# seeds=(0 1 2 3 4)
-# for seed in ${seeds[@]}; do
-#   pixi run python cot.py \
-#     --backend vllm \
-#     --model google/gemma-2-9b-it \
-#     --hf_dtype float16 \
-#     --hf_device_map auto \
-#     --n 2000 --digits  2 4 8 16 32 --kinds add sub mul lcs knap rod ilp_prod ilp_assign ilp_partition \
-#     --outdir out_hf_scale_gemma --exec_code --batch_size 64 --seed ${seed}
-# done
 
 seeds=(1)
 
+
 # for seed in ${seeds[@]}; do
 #   pixi run python cot.py \
 #   --backend vllm \
-#   --model togethercomputer/Pythia-Chat-Base-7B \
+#   --model mistralai/Ministral-8B-Instruct-2410 \
 #   --hf_dtype float16 \
 #   --hf_device_map auto \
 #   --vllm_tensor_parallel 8 \
-#   --n 2000 --digits 2 4 8 16 32 --kinds add sub mul lcs knap rod ilp_prod ilp_assign ilp_partition \
-#   --outdir execution_pythia --exec_code --batch_size 64
+#   --n 100 --digits 2 4 8 16 32 --kinds add sub mul lcs knap rod ilp_prod ilp_assign ilp_partition \
+#   --outdir execution_ministral_smoke --exec_code --batch_size 64
 # done
 
-for seed in ${seeds[@]}; do
-  pixi run python cot.py \
-  --backend vllm \
-  --model mistralai/Ministral-8B-Instruct-2410 \
-  --hf_dtype float16 \
-  --hf_device_map auto \
-  --vllm_tensor_parallel 8 \
-  --n 100 --digits 2 4 8 16 32 --kinds add sub mul lcs knap rod ilp_prod ilp_assign ilp_partition \
-  --outdir execution_ministral_smoke --exec_code --batch_size 64
-done
-
-for seed in ${seeds[@]}; do
-  pixi run python cot.py \
-    --backend vllm \
-    --model deepseek-ai/deepseek-coder-7b-instruct-v1.5 \
-    --hf_dtype float16 \
-    --hf_device_map auto \
-    --vllm_tensor_parallel 8 \
-    --n 100 --digits  2 4 8 16 32 --kinds add sub mul lcs knap rod ilp_prod ilp_assign ilp_partition \
-    --outdir deepseek2_smoke --exec_code --batch_size 64 --seed ${seed}
-done
+# for seed in ${seeds[@]}; do
+#   pixi run python cot.py \
+#     --backend vllm \
+#     --model deepseek-ai/deepseek-coder-7b-instruct-v1.5 \
+#     --hf_dtype float16 \
+#     --hf_device_map auto \
+#     --vllm_tensor_parallel 8 \
+#     --n 100 --digits  2 4 8 16 32 --kinds add sub mul lcs knap rod ilp_prod ilp_assign ilp_partition \
+#     --outdir deepseek2_smoke --exec_code --batch_size 64 --seed ${seed}
+# done
 
 # deepseek-ai/DeepSeek-Coder-V2-Instruct
 for seed in ${seeds[@]}; do
@@ -64,15 +36,15 @@ for seed in ${seeds[@]}; do
     --outdir gemma_smoke1 --exec_code --batch_size 64 --seed ${seed}
 done
 
-for seed in ${seeds[@]}; do
-  pixi run python cot.py \
-    --backend vllm \
-    --model deepseek-ai/DeepSeek-R1-Distill-Llama-8B \
-    --hf_dtype float16 \
-    --hf_device_map auto \
-    --n 100 --digits 2 4 8 16 32 --kinds add sub mul lcs knap rod ilp_prod ilp_assign ilp_partition \
-    --outdir llamadeepseek --exec_code --batch_size 64 --seed ${seed}
-done
+# for seed in ${seeds[@]}; do
+#   pixi run python cot.py \
+#     --backend vllm \
+#     --model deepseek-ai/DeepSeek-R1-Distill-Llama-8B \
+#     --hf_dtype float16 \
+#     --hf_device_map auto \
+#     --n 100 --digits 2 4 8 16 32 --kinds add sub mul lcs knap rod ilp_prod ilp_assign ilp_partition \
+#     --outdir llamadeepseek --exec_code --batch_size 64 --seed ${seed}
+# done
 
 # for seed in ${seeds[@]}; do
 #   pixi run python cot.py \
@@ -85,15 +57,15 @@ done
 #     --outdir deepseekqween --exec_code --batch_size 64 --seed ${seed}
 # done
 
-for seed in ${seeds[@]}; do
-  pixi run python cot.py \
-    --backend vllm \
-    --model deepseek-ai/DeepSeek-R1-Distill-Qwen-32B \
-    --hf_dtype float16 \
-    --hf_device_map auto \
-    --n 100 --digits 2 4 8 16 32 --kinds add sub mul lcs knap rod ilp_prod ilp_assign ilp_partition \
-    --outdir deepseekqween32b --exec_code --batch_size 8 --seed ${seed}
-done
+# for seed in ${seeds[@]}; do
+#   pixi run python cot.py \
+#     --backend vllm \
+#     --model deepseek-ai/DeepSeek-R1-Distill-Qwen-32B \
+#     --hf_dtype float16 \
+#     --hf_device_map auto \
+#     --n 100 --digits 2 4 8 16 32 --kinds add sub mul lcs knap rod ilp_prod ilp_assign ilp_partition \
+#     --outdir deepseekqween32b --exec_code --batch_size 8 --seed ${seed}
+# done
 
 # for seed in ${seeds[@]}; do
 #   pixi run python cot.py \
@@ -115,15 +87,15 @@ done
 #     --outdir gemma3_27b --exec_code --batch_size 1 --seed ${seed}
 # done
 
-for seed in ${seeds[@]}; do
-  pixi run python cot.py \
-    --backend vllm \
-    --model mistralai/Mistral-Small-24B-Instruct-2501 \
-    --hf_dtype float16 \
-    --hf_device_map auto \
-    --n 100 --digits 2 4 8 16 32 --kinds add sub mul lcs knap rod ilp_prod ilp_assign ilp_partition \
-    --outdir mistralsmall --exec_code --batch_size 8 --seed ${seed}
-done
+# for seed in ${seeds[@]}; do
+#   pixi run python cot.py \
+#     --backend vllm \
+#     --model mistralai/Mistral-Small-24B-Instruct-2501 \
+#     --hf_dtype float16 \
+#     --hf_device_map auto \
+#     --n 100 --digits 2 4 8 16 32 --kinds add sub mul lcs knap rod ilp_prod ilp_assign ilp_partition \
+#     --outdir mistralsmall --exec_code --batch_size 8 --seed ${seed}
+# done
 
 # for seed in ${seeds[@]}; do
 #   pixi run python cot.py \
@@ -190,73 +162,5 @@ done
 #     --n 100 --digits  16 --kinds add sub mul lcs knap rod ilp_prod ilp_assign ilp_partition \
 #     --outdir test --exec_code --batch_size 64 --seed 0
 
-# seeds=(0 1 2 3 4)
-# for seed in ${seeds[@]}; do
-#   pixi run python cot.py \
-#     --backend vllm \
-#     --model deepseek-ai/deepseek-coder-7b-instruct-v1.5 \
-#     --hf_dtype float16 \
-#     --hf_device_map auto \
-#     --n 2000 --digits 2 4 8 16 --kinds add sub mul lcs knap rod ilp_prod ilp_assign ilp_partition \
-#     --outdir out_hf_scale_deepseek_code_seeded_nlfix --exec_code --batch_size 64 --seed ${seed}
-# done
-
-# for seed in ${seeds[@]}; do
-#   pixi run python cot.py \
-#     --backend vllm \
-#     --model google/gemma-2-9b-it \
-#     --hf_dtype float16 \
-#     --hf_device_map auto \
-#     --n 2000 --digits 2 4 8 16 32 --kinds add sub mul lcs knap rod ilp_prod ilp_assign ilp_partition \
-#     --outdir execution_gemma --exec_code --batch_size 64 --seed ${seed}
-# done
-
-# for seed in ${seeds[@]}; do
-#   pixi run python cot.py \
-#     --backend vllm \
-#     --vllm_tensor_parallel 6 \
-#     --model Qwen/Qwen2.5-Coder-7B-Instruct \
-#     --hf_dtype float16 \
-#     --hf_device_map auto \
-#     --n 2000 --digits 2 4 8 16 --kinds add sub mul lcs knap rod ilp_prod ilp_assign ilp_partition \
-#     --outdir out_hf_scale_qwen_seeded --exec_code --batch_size 64 --seed ${seed}
-# done
-
-
-# pixi run tensorboard --logdir out_hf/tb
-
-# pixi run python cot.py \
-#   --backend vllm \
-#   --model deepseek-ai/deepseek-llm-7b-chat \
-#   --hf_dtype float16 \
-#   --hf_device_map auto \
-#   --n 10000 --digits 2 4 8 16 32 --kinds add sub mul lcs knap rod ilp_prod ilp_assign ilp_partition \
-#   --outdir out_hf_scale_deepseek_llm --exec_code --batch_size 64
-
-# pixi run python cot.py \
-#   --backend vllm \
-#   --model allenai/OLMo-7B-Instruct-hf \
-#   --hf_dtype float16 \
-#   --hf_device_map auto \
-#   --n 10000 --digits 2 4 8 16 32 --kinds add sub mul lcs knap rod ilp_prod ilp_assign ilp_partition \
-#   --outdir out_hf_scale_olmo --exec_code --batch_size 64
-
-# pixi run python cot.py \
-#   --backend vllm \
-#   --model meta-llama/Llama-3.1-8B-Instruct \
-#   --hf_dtype float16 \
-#   --hf_device_map auto \
-#   --n 10000 --digits 2 4 8 16 32 --kinds add sub mul lcs knap rod ilp_prod ilp_assign ilp_partition \
-#   --outdir out_hf_scale_meta_llama --exec_code --batch_size 64
-
-# pixi run python cot.py \
-#   --backend vllm \
-#   --model Qwen/Qwen2.5-Coder-7B-Instruct \
-#   --hf_dtype float16 \
-#   --hf_device_map auto \
-#   --n 10000 --digits 2 4 8 16 32 --kinds add sub mul lcs knap rod ilp_prod ilp_assign ilp_partition \
-#   --outdir out_hf_scale_qwen --exec_code --batch_size 64
-
-  
 
 
