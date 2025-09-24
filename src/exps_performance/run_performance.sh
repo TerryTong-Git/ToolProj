cb6fe2b3e32752b482c6ba0a2ae9ec3048a35d4b
Terry Tong <dooshtheboss1@gmail.com>
2025-09-23 20:28:07 -0400

    another smoke test

--- src/exps_performance/run_performance.sh
+++ src/exps_performance/run_performance.sh
@@ -32,8 +32,8 @@ for seed in ${seeds[@]}; do
     --model google/gemma-2-9b-it \
     --hf_dtype float16 \
     --hf_device_map auto \
-    --n 100 --digits 2 4 8 16 32 --kinds add sub mul lcs knap rod ilp_prod ilp_assign ilp_partition \
-    --outdir gemma_smoke1 --exec_code --batch_size 64 --seed ${seed}
+    --n 50 --digits 2 4 8 16 32 --kinds add sub mul lcs knap rod ilp_prod ilp_assign ilp_partition \
+    --outdir gemma_smoke3 --exec_code --batch_size 100 --seed ${seed}
 done
 
 # for seed in ${seeds[@]}; do
