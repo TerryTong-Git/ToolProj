0551b948e1a1c78117c9f978a8e7e07ba9ab41ff
Terry Tong <dooshtheboss1@gmail.com>
2025-09-24 14:21:43 -0400

    add logistic regression results for inference

--- src/exps_logistic/run.sh
+++ src/exps_logistic/run.sh
@@ -5,7 +5,7 @@
 #     --label gamma --bits --max_iter 20 --C 0.5
 
 pixi run  python lr_concept_features.py --tbdir /nlpgpu/data/terry/ToolProj/src/exps_performance/out_hf_scale_deepseek_working/tb --rep code \
-    --label gamma --bits --feats hf-cls --embed-model google-bert/bert-base-uncased --max_iter 20 --C 0.5 > bert_code.txt
+    --label gamma --bits --feats hf-cls --embed-model google-bert/bert-base-uncased --max_iter 20 --C 0.5 --feats hf-cls > bert_code.txt
 
 pixi run  python lr_concept_features.py --tbdir /nlpgpu/data/terry/ToolProj/src/exps_performance/out_hf_scale_deepseek_working/tb --rep nl \
-    --label gamma --bits --feats hf-cls --embed-model google-bert/bert-base-uncased --max_iter 20 --C 0.5 > bert_nl.txt
+    --label gamma --bits --feats hf-cls --embed-model google-bert/bert-base-uncased --max_iter 20 --C 0.5 --feats hf-cls > bert_nl.txt 
