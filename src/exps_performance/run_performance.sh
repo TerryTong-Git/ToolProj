42a0cef2855862f79efd3a3e37507eac48e200d0
Terry Tong <dooshtheboss1@gmail.com>
2025-09-24 00:44:31 -0400

    reverting to old prompt + some polishes

--- src/exps_performance/run_performance.sh
+++ src/exps_performance/run_performance.sh
@@ -1,6 +1,6 @@
 
 
-seeds=(1)
+seeds=(0 1 2)
 
 
 # for seed in ${seeds[@]}; do
@@ -26,15 +26,15 @@ seeds=(1)
 # done
 
 # deepseek-ai/DeepSeek-Coder-V2-Instruct
-for seed in ${seeds[@]}; do
-  pixi run python cot.py \
-    --backend vllm \
-    --model google/gemma-2-9b-it \
-    --hf_dtype float16 \
-    --hf_device_map auto \
-    --n 50 --digits 2 4 8 16 32 --kinds add sub mul lcs knap rod ilp_prod ilp_assign ilp_partition \
-    --outdir gemma_smoke3 --exec_code --batch_size 100 --seed ${seed}
-done
+# for seed in ${seeds[@]}; do
+#   pixi run python cot.py \
+#     --backend vllm \
+#     --model google/gemma-2-9b-it \
+#     --hf_dtype float16 \
+#     --hf_device_map auto \
+#     --n 50 --digits 2 4 8 16 32 --kinds add sub mul lcs knap rod ilp_prod ilp_assign ilp_partition \
+#     --outdir gemma_smoke3 --exec_code --batch_size 100 --seed ${seed}
+# done
 
 # for seed in ${seeds[@]}; do
 #   pixi run python cot.py \
@@ -57,15 +57,15 @@ done
 #     --outdir deepseekqween --exec_code --batch_size 64 --seed ${seed}
 # done
 
-# for seed in ${seeds[@]}; do
-#   pixi run python cot.py \
-#     --backend vllm \
-#     --model deepseek-ai/DeepSeek-R1-Distill-Qwen-32B \
-#     --hf_dtype float16 \
-#     --hf_device_map auto \
-#     --n 100 --digits 2 4 8 16 32 --kinds add sub mul lcs knap rod ilp_prod ilp_assign ilp_partition \
-#     --outdir deepseekqween32b --exec_code --batch_size 8 --seed ${seed}
-# done
+for seed in ${seeds[@]}; do
+  pixi run python cot.py \
+    --backend vllm \
+    --model deepseek-ai/DeepSeek-R1-Distill-Qwen-32B \
+    --hf_dtype float16 \
+    --hf_device_map auto \
+    --n 250 --digits 2 4 8 16 32 --kinds add sub mul lcs knap rod ilp_prod ilp_assign ilp_partition \
+    --outdir deepseekqween32b --exec_code --batch_size 8 --seed ${seed}
+done
 
 # for seed in ${seeds[@]}; do
 #   pixi run python cot.py \
@@ -87,15 +87,15 @@ done
 #     --outdir gemma3_27b --exec_code --batch_size 1 --seed ${seed}
 # done
 
-# for seed in ${seeds[@]}; do
-#   pixi run python cot.py \
-#     --backend vllm \
-#     --model mistralai/Mistral-Small-24B-Instruct-2501 \
-#     --hf_dtype float16 \
-#     --hf_device_map auto \
-#     --n 100 --digits 2 4 8 16 32 --kinds add sub mul lcs knap rod ilp_prod ilp_assign ilp_partition \
-#     --outdir mistralsmall --exec_code --batch_size 8 --seed ${seed}
-# done
+for seed in ${seeds[@]}; do
+  pixi run python cot.py \
+    --backend vllm \
+    --model mistralai/Mistral-Small-24B-Instruct-2501 \
+    --hf_dtype float16 \
+    --hf_device_map auto \
+    --n 250 --digits 2 4 8 16 32 --kinds add sub mul lcs knap rod ilp_prod ilp_assign ilp_partition \
+    --outdir mistralsmall --exec_code --batch_size 8 --seed ${seed}
+done
 
 # for seed in ${seeds[@]}; do
 #   pixi run python cot.py \
