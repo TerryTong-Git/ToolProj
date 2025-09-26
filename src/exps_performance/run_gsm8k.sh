d594b6ea28ffb390436e3c2f5850273dd1e0a701
Terry Tong <dooshtheboss1@gmail.com>
2025-09-26 13:52:09 -0400

    added finalized prompt, ready to scale up

--- src/exps_performance/run_gsm8k.sh
+++ src/exps_performance/run_gsm8k.sh
@@ -2,17 +2,70 @@
 
 seeds=(0)
 
+# for seed in ${seeds[@]}; do
+#   pixi run python cot.py \
+#     --backend vllm \
+#     --model deepseek-ai/deepseek-coder-7b-instruct-v1.5 \
+#     --hf_dtype float16 \
+#     --hf_device_map auto \
+#     --vllm_tensor_parallel 8 \
+#     --kinds gsm8k \
+#     --outdir gsm8k --exec_code --batch_size 64 --seed ${seed}
+# done
+
+
 for seed in ${seeds[@]}; do
   pixi run python cot.py \
     --backend vllm \
-    --model deepseek-ai/deepseek-coder-7b-instruct-v1.5 \
+    --model google/gemma-2-9b-it \
     --hf_dtype float16 \
     --hf_device_map auto \
-    --vllm_tensor_parallel 8 \
     --kinds gsm8k \
-    --outdir gsm8k --exec_code --batch_size 64 --seed ${seed}
+    --outdir gemma_gsm8k --exec_code --batch_size 64 --seed ${seed}
 done
 
+
+
+for seed in ${seeds[@]}; do
+  pixi run python cot.py \
+  --backend vllm \
+  --model mistralai/Ministral-8B-Instruct-2410 \
+  --hf_dtype float16 \
+  --hf_device_map auto \
+  --vllm_tensor_parallel 8 \
+  --kinds gsm8k \
+  --outdir ministral_gsm8k --exec_code --batch_size 64
+done
+
+for seed in ${seeds[@]}; do
+  pixi run python cot.py \
+    --backend vllm \
+    --model deepseek-ai/DeepSeek-R1-Distill-Qwen-32B \
+    --hf_dtype float16 \
+    --hf_device_map auto \
+    --kinds gsm8k \
+    --outdir qwen_gsm8k --exec_code --batch_size 8 --seed ${seed}
+done
+
+for seed in ${seeds[@]}; do
+  pixi run python cot.py \
+    --backend vllm \
+    --model mistralai/Mistral-Small-24B-Instruct-2501 \
+    --hf_dtype float16 \
+    --hf_device_map auto \
+    --kinds gsm8k \
+    --outdir mistral_gsm8k --exec_code --batch_size 8 --seed ${seed}
+done
+
+for seed in ${seeds[@]}; do
+  pixi run python cot.py \
+    --backend vllm \
+    --model deepseek-ai/DeepSeek-R1-Distill-Llama-8B \
+    --hf_dtype float16 \
+    --hf_device_map auto \
+    --n 100 --digits 2 4 8 16 32 --kinds add sub mul lcs knap rod ilp_prod ilp_assign ilp_partition \
+    --outdir llama_reasoning --exec_code --batch_size 64 --seed ${seed}
+done
 
   
 
