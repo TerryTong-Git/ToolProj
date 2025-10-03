dbd63bafe28e8d5f7847e02dd25de84983932b33
Terry Tong <dooshtheboss1@gmail.com>
2025-10-03 18:03:27 -0400

    correlations look good

--- src/exps_correlation/run_corr.sh
+++
@@ -1,9 +0,0 @@
-seeds=(1 2 3 4)
-
-for seed in ${seeds[@]}; do
-    pixi run python run_corr.py --models meta-llama/Llama-3.1-8B-Instruct --engine vllm --temperature 0.3 --top_p 0.95 --seed ${seed}
-    pixi run python run_corr.py --models deepseek-ai/DeepSeek-R1-Distill-Llama-8B --engine vllm --temperature 0.3 --top_p 0.95 --seed ${seed}
-    pixi run python run_corr.py --models google/gemma-2-9b-it --engine vllm --temperature 0.3 --top_p 0.95 --seed ${seed}
-    pixi run python run_corr.py --models deepseek-ai/deepseek-coder-7b-instruct-v1.5  --engine vllm --temperature 0.3 --top_p 0.95 --seed ${seed}
-    pixi run python run_corr.py --models mistralai/Ministral-8B-Instruct-2410 --engine vllm --temperature 0.3 --top_p 0.95 --seed ${seed}
-done
