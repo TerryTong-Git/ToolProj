840f4ca73bc74c1ffbd61874b8be0b96ec8e7f9c
Terry Tong <dooshtheboss1@gmail.com>
2025-10-13 20:33:47 -0400

    added Langchan query visualization, can scale up tn

--- src/exps_pret_corr/filter.py
+++ src/exps_pret_corr/filter.py
@@ -2,22 +2,22 @@
 import cProfile
 import io
 import logging
-import multiprocessing
 import os
 import pstats
 from pathlib import Path
 from pstats import SortKey
-from typing import List
+from typing import Dict, List
 
 import matplotlib.pyplot as plt
 import numpy as np
+import pandas as pd
 from datasets import Dataset, load_dataset
-from fastembed import TextEmbedding
-from joblib import Parallel, delayed
 from langchain.docstore.document import Document
 from langchain.text_splitter import RecursiveCharacterTextSplitter
-from sklearn.cluster import KMeans
-from sklearn.manifold import TSNE
+from langchain_community.embeddings import HuggingFaceEmbeddings
+from langchain_community.vectorstores import FAISS
+from langchain_community.vectorstores.utils import DistanceStrategy
+from pydantic import BaseModel
 from tqdm import tqdm
 from transformers import AutoTokenizer
 
@@ -41,25 +41,63 @@ MARKDOWN_SEPARATORS = [
     "",
 ]
 
+QUERIES = [
+    "rod cutting",
+    "longest common subsequence",
+    "knapsack",
+    "addition",
+    "subtraction",
+    "multiplication",
+    "integer linear programming production problem",
+    "integer linear programming assignment problem",
+    "integer linear programming partition problem",
+]
 
-def add_task(text_splitter: RecursiveCharacterTextSplitter, doc: Document) -> None:
-    return text_splitter.split_documents([doc])
+
+class DataRecord(BaseModel):
+    name: str
+    path: Path  # something like text, parquet etc.
+    file_type: str
+    column_name: str
+    source_path: str
+    source_path_pattern: str
 
 
-def load_datasets(tokenizer_name: str) -> dict:
-    starcoder_data = load_dataset("parquet", data_dir=starcoder_path)
-    # starcoder_data_iter = starcoder_data.to_iterable_dataset()
-    dclm_data = load_dataset("text", data_dir=dclm_path)
-    # dclm_data_iter = dclm_data.to_iterable_dataset()
-    pr = cProfile.Profile()
-    pr.enable()
+def add_task(text_splitter: RecursiveCharacterTextSplitter, doc: Document) -> Document:
+    return text_splitter.split_documents([doc])[0]
 
-    # RAW_KNOWLEDGE_BASE= Parallel(n_jobs=-1)(
-    #     delayed(Document)(page_content=doc["content"]) for doc in tqdm(starcoder_data['train'])
+
+def convert_to_docs(data, column_name, text_splitter) -> List[Document]:
+    RAW_KNOWLEDGE_BASE: List[Document] = [
+        Document(page_content=doc[column_name]) for doc in tqdm(data)
+    ]  ## so far this is faster, what if we scale up?
+    RAW_KNOWLEDGE_BASE = RAW_KNOWLEDGE_BASE[:10]  # debugging
+    # cpus = multiprocessing.cpu_count()
+    # logger.info(f"Utilizing {cpus} CPUS in splitting text")
+    # processed_docs: List[Document] = Parallel(jobs=cpus, backend="threading")(
+    #     delayed(add_task)(text_splitter, doc) for doc in tqdm(RAW_KNOWLEDGE_BASE, desc="splitting documents")
     # )
+    # logger.info(
+    #     f"Number of docs before: {len(RAW_KNOWLEDGE_BASE)}, \
+    #     Number of docs after: {len(processed_docs)}"
+    # )
+    processed_docs = RAW_KNOWLEDGE_BASE
+    return processed_docs
 
+
+def dedup_docs(processed_docs: List[Document]) -> List[Document]:
+    unique_texts = {}
+    dedupped_docs = []
+    for doc in tqdm(processed_docs, desc="deduplicating"):
+        if doc.page_content not in unique_texts:
+            unique_texts[doc.page_content] = True
+            dedupped_docs.append(doc)
+    return dedupped_docs
+
+
+def load_datasets(to_load: List[DataRecord], tokenizer: AutoTokenizer, profile: bool = False) -> Dict[str, List[Document]]:
     text_splitter: RecursiveCharacterTextSplitter = RecursiveCharacterTextSplitter.from_huggingface_tokenizer(
-        tokenizer=AutoTokenizer.from_pretrained(tokenizer_name),
+        tokenizer=tokenizer,
         chunk_size=1000,
         chunk_overlap=100,
         add_start_index=True,
@@ -67,72 +105,42 @@ def load_datasets(tokenizer_name: str) -> dict:
         separators=MARKDOWN_SEPARATORS,
     )
 
-    RAW_KNOWLEDGE_BASE: List[Document] = [
-        Document(page_content=doc["content"]) for doc in tqdm(starcoder_data["train"])
-    ]  ## so far this is faster, what if we scale up?
+    result: Dict[str, List[Document]] = {}
+    for dataRecord in tqdm(to_load, desc="loading data"):
+        dataset = load_dataset(dataRecord.file_type, data_dir=dataRecord.path)
 
-    cpus = multiprocessing.cpu_count()
-    logger.info(f"Utilizing {cpus} CPUS in splitting text")
-    processed_docs: List[Document] = Parallel(jobs=cpus, backend="threading")(
-        delayed(add_task)(text_splitter, doc) for doc in tqdm(RAW_KNOWLEDGE_BASE, desc="splitting documents")
-    )
+        if profile:
+            pr = cProfile.Profile()
+            pr.enable()
 
-    # processed_docs: List[Document] = []
-    # with concurrent.futures.ProcessPoolExecutor(max_workers = cpus) as executor:
-    #     task = partial(add_task, text_splitter = text_splitter)
-    #     for result in tqdm(executor.map(task, RAW_KNOWLEDGE_BASE), desc="splitting documents"):
-    #         processed_docs += result
+        processed_docs: List[Document] = convert_to_docs(dataset["train"], dataRecord.column_name, text_splitter)
 
-    unique_texts = {}
-    dedupped_docs = []
-    for doc in tqdm(processed_docs, desc="deduplicating"):
-        if doc.page_content not in unique_texts:
-            unique_texts[doc.page_content] = True
-            dedupped_docs.append(doc)
-    logger.info(
-        f"Number of docs before: {len(RAW_KNOWLEDGE_BASE)}, \
-        Number of docs after: {len(processed_docs)}, \
-        Number of docs after dedup: {len(dedupped_docs)}"
-    )
+        # import pdb; pdb.set_trace() # sus doc types
 
-    pr.disable()
-    s = io.StringIO()
-    sortby = SortKey.CUMULATIVE
-    ps = pstats.Stats(pr, stream=s).sort_stats(sortby)
-    ps.print_stats()
-    stats = s.getvalue()
+        # deduped_docs: List[Document] = dedup_docs(processed_docs)
+        # logger.info(
+        #     f"Number of docs before dedup: {len(processed_docs)}, \
+        #     Number of docs after dedup: {len(deduped_docs)} "
+        # )
+        deduped_docs = processed_docs
 
-    with open("stats.txt", "w+") as f:
-        f.write(stats)
-    data = {"STARCODER": starcoder_data["train"]["content"], "DCLM": dclm_data["train"]["text"]}
-    return data
+        if profile:
+            pr.disable()
+            s = io.StringIO()
+            sortby = SortKey.CUMULATIVE
+            ps = pstats.Stats(pr, stream=s).sort_stats(sortby)
+            ps.print_stats()
+            stats = s.getvalue()
+
+            with open(f"stats_{dataRecord.name}.txt", "w+") as f:
+                f.write(stats)
+        result[dataRecord.name] = deduped_docs
+    return result
 
 
 def serialize_filtered_datasets(data: Dataset, datapath: Path) -> None:
     data.save_to_disk(datapath)
     logging.info(f"Saved Dataset to: {datapath.name}")
-
-
-def embed_func(model: TextEmbedding, data: List[str], batch_size: int, parallel: int) -> List:
-    """
-    Transforms a list of inputs into a list of text embedding vectors
-    """
-    return list([e for e in tqdm(model.embed(data, batch_size=batch_size, parallel=parallel), total=len(data))])
-
-
-def kmeans(embeddings: List) -> List[int]:
-    model = KMeans(n_clusters=2)
-    model.fit(embeddings)
-    return model.labels_
-
-
-def tsne_visualize(embeddings, labels):
-    tsne = TSNE(n_components=2)
-    X = tsne.fit_transform(np.array(embeddings))
-    plt.plot((8, 6))
-    plt.scatter(X[:, 0], X[:, 1], c=labels, cmap="viridis", marker="o")
-    plt.show()
-    plt.savefig("tsne.png")
 
 
 def get_data_statistics():
@@ -155,18 +163,121 @@ def evaluate():
     pass
 
 
-def main():
-    model_name = "sentence-transformers/all-MiniLM-L6-v2"
-    data = load_datasets(model_name)
-    device_ids = [0, 1, 2, 3, 4, 5, 6, 7]
-    logger.info("Starting to Load Text Model ... ")
-    embed_model = TextEmbedding(model_name=model_name, cuda=True, device_ids=device_ids, lazy_load=True)
-    logger.info("Finished Loading Text Embedding model ... ")
-    embedded_text = embed_func(embed_model, data["STARCODER"], 64, len(device_ids))
+def plot_length_distribution(tokenizer: AutoTokenizer, data: Dict[str, List[Document]]) -> None:
+    for name, documents in data.items():
+        lengths = [len(tokenizer.encode(doc.page_content)) for doc in tqdm(documents, desc="processing_docs")]
+        pd.Series(lengths).hist()
+        plt.title(f"{name} plot of length distribution")
+        plt.show()
+        plt.savefig(f"{name}_lengths.png")
+        plt.close()
 
-    ##TODO retrieve topk documents about algorithm X. Use that.
-    labels = kmeans(embedded_text)
-    tsne_visualize(embedded_text, labels)
+
+# add types and docstrings
+def query_viz(vectordb: FAISS, queries: List[str], processed_docs: List[Document], embedding_model: HuggingFaceEmbeddings) -> None:
+    """
+    Creates a visualization with the query as a star to see where it lives in the embeddings
+
+    Params:
+        vectordb: FAISS approximate nearest neighbor algorithm
+        queries: list of algorithms we are trying to retrieve
+        processed_docs: List[Document] of documents we have chunked and are ready to retrieve
+        embedding_model: HuggingFaceEmbeddings, defaults to sentence-transformers minilm
+
+    Returns:
+        None. However, as a side effect, generates a visualization of queries alongside other documents
+    """
+    import pacmap
+    import plotly.express as px
+
+    embedding_proj = pacmap.PaCMAP(n_components=2, n_neighbors=2, MN_ratio=0.5, FP_ratio=0.5, random_state=1)
+
+    logger.info("Starting Embedding Query")
+    embedded_queries = [embedding_model.embed_query(query) for query in queries]
+    logger.info("Starting to index")
+    embeddings_2d = [list(vectordb.index.reconstruct_n(idx, 1)[0]) for idx in range(len(processed_docs))] + [query for query in embedded_queries]
+    logger.info("Starting to project")
+    proj_docs = embedding_proj.fit_transform(np.array(embeddings_2d), init="pca")
+    logger.info("Plotting")
+    # import pdb; pdb.set_trace()
+    df = [
+        {
+            "x": proj_docs[i, 0],
+            "y": proj_docs[i, 1],
+            "source": "",
+            "extract": processed_docs[i].page_content[:100] + "...",
+            "symbol": "circle",
+            "size_col": 4,
+        }
+        for i in range(len(processed_docs))
+    ] + [
+        {
+            "x": proj_docs[len(processed_docs) + i, 0],
+            "y": proj_docs[len(processed_docs) + i, 1],
+            "source": "User Query",
+            "extract": queries[i],
+            "symbol": "star",
+            "size_col": 100,
+        }
+        for i in range(len(queries))
+    ]
+    fig = px.scatter(
+        df,
+        x="x",
+        y="y",
+        color="source",
+        hover_data="extract",
+        size="size_col",
+        symbol="symbol",
+        color_discrete_map={"User Query": "black"},
+        width=1000,
+        height=700,
+    )
+    fig.update_traces(marker=dict(opacity=1, line=dict(width=0, color="DarkSlateGrey")), selector=dict(mode="markers"))
+    # fig.show()
+    fig.write_image("queryscatter.png")
+    logger.info("Finished saving")
+
+
+def search(embedding_model: HuggingFaceEmbeddings, data: Dict[str, List[Document]]) -> None:
+    for name, documents in data.items():
+        VECTORDB = FAISS.from_documents(documents, embedding_model, distance_strategy=DistanceStrategy.COSINE)
+        logger.info(f"Creating Visualizaiton for {name}")
+        query_viz(VECTORDB, QUERIES, documents, embedding_model)
+        VECTORDB.save_local("faiss_index")
+
+
+def main() -> None:
+    model_name = "sentence-transformers/all-MiniLM-L6-v2"
+    to_load = [
+        DataRecord(
+            name="starcoder",
+            path=Path(starcoder_path),
+            column_name="content",
+            source_path="max_stars_repo_path",
+            source_path_pattern=r"",
+            file_type="parquet",
+        ),
+        DataRecord(
+            name="dclm",
+            path=Path(dclm_path),
+            column_name="text",
+            source_path="fasttext_openhermes_reddit_eli5_vs_rw_v2_bigram_200k_train_prob",
+            source_path_pattern=r"(0.\d{2})",
+            file_type="text",
+        ),
+    ]
+    tokenizer: AutoTokenizer = AutoTokenizer.from_pretrained(model_name)
+    data: Dict[str, List[Document]] = load_datasets(to_load, tokenizer, profile=True)
+    plot_length_distribution(tokenizer, data)
+
+    EMBEDDING_MODEL = HuggingFaceEmbeddings(
+        model_name=model_name,
+        multi_process=True,
+        model_kwargs={"device": "cuda", "device_map": "auto"},
+        encode_kwargs={"normalize_embeddings": True, "batch_size": 1024},
+    )
+    search(EMBEDDING_MODEL, data)
 
 
 if __name__ == "__main__":
